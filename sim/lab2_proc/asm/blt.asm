4e99cb61f51b60f91ec9d87d9791ab6d7d389d60
gpm58 <gpm58@cornell.edu>
2023-10-14 03:16:02 -0400

    Basic Tests Done (Might add more later?)

---
+++ sim/lab2_proc/asm/blt.asm
@@ -0,0 +1,11 @@
+csrr x1, mngr2proc < 5
+csrr x2, mngr2proc < 4
+csrr x2, mngr2proc < 6
+blt x2, x3, branch_target
+addi x1, x1, 1
+
+branch_target:
+csrr x1, mngr2proc < 5
+
+
+
---
+++ sim/lab2_proc/asm/xori1.asm
@@ -0,0 +1,3 @@
+csrr x1, mngr2proc < 255
+xor x3, x1, 255
+csrw proc2mngr, x3 > 0
